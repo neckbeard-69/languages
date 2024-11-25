de9966926021afe25eae82312942e72326ae79d7
Kyle Lin <minecraft.kyle.train@gmail.com>
2024-11-26 02:43:21 +0800

    Remove redundant function

--- loops/haskell/code.hs
+++ loops/haskell/code.hs
@@ -3,11 +3,6 @@ module Main where
 import System.Environment (getArgs)
 import System.Random (randomRIO)
 
-fibonacci :: Int -> Int
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
-
 main :: IO ()
 main = do
   u <- fmap (subtract 1 . read . head) getArgs
