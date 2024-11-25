38058f799ac6eb8948f02db5f8a0a83779f07efe
Vishrut Jha <vishrutkmr7@icloud.com>
2024-11-25 13:09:15 -0700

    Refactor Fibonacci function for improved readability and consistency in JavaScript; use const and formatted conditions

--- fibonacci/js/code.js
+++ fibonacci/js/code.js
@@ -1,12 +1,15 @@
-
 function fibonacci(n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  return fibonacci(n-1) + fibonacci(n-2);
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-var u = Number(process.argv[2]);
-var r = 0;
+const u = Number(process.argv[2]);
+const r = 0;
 for (let i = 1; i < u; i++) {
   r += fibonacci(i);
 }
