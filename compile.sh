796df80cdb0ff07d23ee7262fe10f81cbbb5db2f
Paulo-21 <paul.cibier@gmail.com>
2024-11-26 18:19:53 +0000

    Fix the unfair betwenn C and Rust code and use stable toolchain

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 clang -O3 c/code.c -o c/code
 go build -o go/code go/code.go
 javac jvm/code.java
-RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
+cargo build --manifest-path rust/Cargo.toml --release
 kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
 #kotlinc-native -include-runtime kotlin/code.kt -d kotlin/code
 dart compile exe dart/code.dart -o dart/code --target-os=macos
