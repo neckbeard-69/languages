f7fa47a99c31cb8c0197e43db817ea45b5b0ee93
Vishrut Jha <vishrutkmr7@icloud.com>
2024-11-25 17:10:29 -0700

    python lint issues

--- fibonacci/py/code.py
+++ fibonacci/py/code.py
@@ -4,11 +4,11 @@ def fibonacci(n):
   if (n == 0):
     return 0
   if (n == 1):
-     return 1
+    return 1
   return fibonacci(n-1) + fibonacci(n-2)
 
 u = int(sys.argv[1])
 r = 0
 for i in range(u):
-   r += fibonacci(i)
+  r += fibonacci(i)
 print(r)
