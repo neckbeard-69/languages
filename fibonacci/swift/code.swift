1891bece483d53568adeef236320645acc589927
Vishrut Jha <vishrutkmr7@icloud.com>
2024-11-26 02:25:32 -0700

    revert from UInt to Int

--- fibonacci/swift/code.swift
+++ fibonacci/swift/code.swift
@@ -1,11 +1,11 @@
-func fibonacci(_ n: UInt) -> UInt {
+func fibonacci(_ n: Int) -> Int {
     if n == 0 { return 0 }
     if n == 1 { return 1 }
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
-let u = UInt(CommandLine.arguments[1])!
-var r: UInt = 0
+let u = Int(CommandLine.arguments[1])!
+var r = 0
 
 for i in 1..<u {
     r += fibonacci(i)
--- loops/swift/code.swift
+++ loops/swift/code.swift
@@ -1,12 +1,12 @@
-let u = UInt(CommandLine.arguments[1])!  // Get input number from command line
-let r = UInt.random(in: 0..<10000)  // Get random number 0 <= r < 10k
-var a: [UInt] = Array(repeating: 0, count: 10000)  // Array of 10k elements initialized to 0
+let u = Int(CommandLine.arguments[1])!  // Get input number from command line
+let r = Int.random(in: 0..<10000)  // Get random number 0 <= r < 10k
+var a: [Int] = Array(repeating: 0, count: 10000)  // Array of 10k elements initialized to 0
 
 for i in 0..<10000 {  // 10k outer loop iterations
     for j in 0..<100000 {  // 100k inner loop iterations
-        a[i] += UInt(j) % u  // Simple sum
+        a[i] += j % u  // Simple sum
     }
     a[i] += r  // Add random value to each element
 }
 
-print(a[Int(r)])  // Print single element from array
+print(a[r])  // Print single element from array
