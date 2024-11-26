a80e6ff93496e90664d39495e440d8a078fad047
Vishrut Jha <vishrutkmr7@icloud.com>
2024-11-26 15:45:50 -0700

    Refactor Swift code to use structured main and static methods

--- fibonacci/swift/code.swift
+++ fibonacci/swift/code.swift
@@ -1,14 +1,19 @@
-func fibonacci(_ n: Int) -> Int {
-    if n == 0 { return 0 }
-    if n == 1 { return 1 }
-    return fibonacci(n - 1) + fibonacci(n - 2)
+@main
+struct Main {
+    static func fibonacci(_ n: Int) -> Int {
+        if n == 0 { return 0 }
+        if n == 1 { return 1 }
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    }
+
+    static func main() {
+        let u = Int(CommandLine.arguments[1])!
+        var r = 0
+
+        for i in 1..<u {
+            r += fibonacci(i)
+        }
+
+        print(r)
+    }
 }
-
-let u = Int(CommandLine.arguments[1])!
-var r = 0
-
-for i in 1..<u {
-    r += fibonacci(i)
-}
-
-print(r)
--- loops/swift/code.swift
+++ loops/swift/code.swift
@@ -1,12 +1,17 @@
-let u = Int(CommandLine.arguments[1])!  // Get input number from command line
-let r = Int.random(in: 0..<10000)  // Get random number 0 <= r < 10k
-var a: [Int] = Array(repeating: 0, count: 10000)  // Array of 10k elements initialized to 0
+@main
+struct Main {
+    static func main() {
+        let u = Int(CommandLine.arguments[1])!  // Get input number from command line
+        let r = Int.random(in: 0..<10000)  // Get random number 0 <= r < 10k
+        var a: [Int] = Array(repeating: 0, count: 10000)  // Array of 10k elements initialized to 0
 
-for i in 0..<10000 {  // 10k outer loop iterations
-    for j in 0..<100000 {  // 100k inner loop iterations
-        a[i] += j % u  // Simple sum
+        for i in 0..<10000 {  // 10k outer loop iterations
+            for j in 0..<100000 {  // 100k inner loop iterations
+                a[i] = a[i] &+ (j % u)  // Simple sum
+            }
+            a[i] = a[i] &+ r  // Add random value to each element
+        }
+
+        print(a[r])  // Print single element from array
     }
-    a[i] += r  // Add random value to each element
 }
-
-print(a[r])  // Print single element from array
