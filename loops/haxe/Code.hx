c0c180489e3a65f9f6874e6bdd8eae999b9adf79
Barış Yıldırım <fb_bcy@hotmail.com>
2024-11-26 22:07:31 +0300

    Improve performance for JVM

--- loops/haxe/Code.hx
+++ loops/haxe/Code.hx
@@ -4,10 +4,13 @@ class Code {
 	public static function main() {
 		var u:Int = Std.parseInt(Sys.args()[0]); // Command line input number
 		var r:Int = Std.random(10000);
-		var a = new Vector<Int>(10000); // Initialize a vector with 10,000 elements, all set to 0
-		a.fill(0);
+		var a = new Vector<Int>(10000); // Initialize a vector with 10,000 elements
 
 		for (i in 0...10000) { // Outer loop with 10k iterations
+			#if !java
+			a[i] = 0; // Set all VectorData to 0, this part is unnecessary since the JVM target already fills the values with 0.
+			#end
+			
 			for (j in 0...100000) { // 100k inner loop iterations
 				a[i] = a[i] + j % u; // Simple sum operation
 			}
