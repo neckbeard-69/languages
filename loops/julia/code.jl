e244ce72772ff44f813ee9e5ed9a26691e26749f
marinlauber <marinlauber@tudelft.nl>
2024-11-26 07:25:33 +0100

    mod in loop code

--- loops/julia/code.jl
+++ loops/julia/code.jl
@@ -4,7 +4,7 @@ let
     r = only(rand(1:10^4,1))
     @inbounds for i ∈ 1:10^4
         @inbounds for j ∈ 1:10^5
-            a[i] = a[i] + u
+            a[i] = a[i] + j%u 
         end
         a[i] = a[i] + r
     end
