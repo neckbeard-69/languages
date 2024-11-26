cca44b0ad5bd2a38009c2ad6de145160a99f7abd
marinlauber <marinlauber@tudelft.nl>
2024-11-26 08:31:17 +0100

    simplify random number generation

--- loops/julia/code.jl
+++ loops/julia/code.jl
@@ -1,7 +1,7 @@
 let
     a = zeros(Int,10^4)
     u = parse(Int,ARGS[1])
-    r = only(rand(1:10^4,1))
+    r = rand(1:10^4)
     @inbounds for i ∈ 1:10^4
         @inbounds for j ∈ 1:10^5
             a[i] = a[i] + j%u 
