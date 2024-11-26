85ea0476a4b4ab08f045af5008a6320db57aea3a
Paul Nathan <paul@nathan.house>
2024-11-25 18:16:48 -0800

    Common Lisp: Loop Contender.
    The Common Lisp system used is SBCL, which has gotten the bulk of the
    performance improvements for a long time.
    
    The code has been tuned, but plausibly there might be other speedups possible.

---
+++ loops/common-lisp/code.lisp
@@ -0,0 +1,26 @@
+#-sbcl (error "only Steel Bank Common Lisp supported, sorry")
+
+(defun main ()
+  (declare (optimize (speed 3) (safety 0)))
+  (let ((r (random 10000 (make-random-state t)))
+        (a (make-array 10000   :element-type '(unsigned-byte 32) ))
+        (u (parse-integer (second *posix-argv*)) ))
+    (declare
+     (type (mod 10000) r)
+     (type (unsigned-byte 32) u)
+     (type (simple-array (unsigned-byte 32) (10000)) a))
+
+
+    (loop for i of-type (mod 10001) from 0 below 10000 do
+      (loop for j of-type (mod 10001) from 0 below 10000 do
+        (setf (aref a i) (mod j u)))
+      (setf (aref a i) (+ (aref a i) r)))
+    (format t "~a" (aref a r))))
+
+;; compile or run depending on args and situation.
+(if (member "--build" sb-ext:*posix-argv* :test #'string=)
+    (sb-ext:save-lisp-and-die "./common-lisp/code"
+                            :toplevel #'main
+                            :executable t
+                            :save-runtime-options t)
+    (main))
