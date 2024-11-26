63367cad089aa4e60755da72d8aa13b8c6a7e744
Vishrut Jha <vishrutkmr7@icloud.com>
2024-11-25 17:08:54 -0700

    Unlint

--- fibonacci/py/code.py
+++ fibonacci/py/code.py
@@ -3,8 +3,12 @@ import sys
 def fibonacci(n):
   if (n == 0):
     return 0
-  return 1 if (n == 1) else fibonacci(n-1) + fibonacci(n-2)
+  if (n == 1):
+     return 1
+  return fibonacci(n-1) + fibonacci(n-2)
 
 u = int(sys.argv[1])
-r = sum(fibonacci(i) for i in range(u))
+r = 0
+for i in range(u):
+   r += fibonacci(i)
 print(r)
