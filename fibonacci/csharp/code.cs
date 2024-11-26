cd77d5110d071065ea1f82d1d6dce3e8b6a7efe0
Noctre <17685682+Noctree@users.noreply.github.com>
2024-11-26 20:20:34 +0100

    Sticking to Allman bracket style
    loops/code.cs
    Removed unnecessary comments
    variable 'a' is explicitly typed as a Span<int>
    Using Random.Shared instead of 'new Random()'
    Using a.Length as a control variable for the outer loop
    
    fibonacci/code.cs
    Rewrote the Fibonacci method into a ternary operator

--- fibonacci/csharp/code.cs
+++ fibonacci/csharp/code.cs
@@ -1,18 +1,10 @@
-﻿static int Fibonacci(int n) {
-    if (n == 0) {
-        return 0;
-    }
-
-    if (n == 1) {
-        return 1;
-    }
-
-    return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
+﻿static int Fibonacci(int n) =>
+    n <= 1 ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
 
 var u = int.Parse(args[0]);
 var r = 0;
-for (var i = 1; i < u; i++) {
+for (var i = 1; i < u; i++)
+{
     r += Fibonacci(i);
 }
 Console.WriteLine(r);
--- loops/csharp/code.cs
+++ loops/csharp/code.cs
@@ -1,10 +1,16 @@
-﻿var u = int.Parse(args[0]); // Get an input number from the command line
-var r = new Random().Next(10000); // Get a random number 0 <= r < 10k
-var a = new int[10000]; // Array of 10k elements initialized to 0
-for (var i = 0; i < 10000; i++) { // 10k outer loop iterations
-    for (var j = 0; j < 100000; j++) { // 100k inner loop iterations, per outer loop iteration
-        a[i] = a[i] + j % u; // Simple sum
+﻿var u = int.Parse(args[0]);
+var r = Random.Shared.Next(10_000);
+
+//Use span instead of array
+Span<int> a = new int[10_000];
+
+//using a.Length instead of 10_000 constant should give the compiler better optimization hints
+for (var i = 0; i < a.Length; i++)
+{
+    for (var j = 0; j < 100_000; j++)
+    {
+        a[i] = a[i] + j % u;
     }
-    a[i] += r; // Add a random value to each element in array
+    a[i] += r;
 }
-Console.WriteLine(a[r]); // Print out a single element from the array
+Console.WriteLine(a[r]);
