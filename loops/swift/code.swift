21e87eafc490100089e0faa7f3b7e573debf2533
Vishrut Jha <vishrutkmr7@icloud.com>
2024-11-25 17:52:54 -0700

    Type changes

--- loops/swift/code.swift
+++ loops/swift/code.swift
@@ -1,12 +1,12 @@
 let u = UInt(CommandLine.arguments[1])!  // Get input number from command line
 let r = UInt.random(in: 0..<10000)  // Get random number 0 <= r < 10k
-var a = Array(repeating: 0, count: 10000)  // Array of 10k elements initialized to 0
+var a: [UInt] = Array(repeating: 0, count: 10000)  // Array of 10k elements initialized to 0
 
 for i in 0..<10000 {  // 10k outer loop iterations
     for j in 0..<100000 {  // 100k inner loop iterations
-        a[i] += j % Int(u)  // Simple sum
+        a[i] += UInt(j) % u  // Simple sum
     }
-    a[i] += Int(r)  // Add random value to each element
+    a[i] += r  // Add random value to each element
 }
 
 print(a[Int(r)])  // Print single element from array
