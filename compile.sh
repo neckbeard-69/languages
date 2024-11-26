ebd7d310f2ff65f27fc24a9f25bd9ffe79869fd8
German David <germanmarinrolong@gmail.com>
2024-11-26 00:52:55 -0500

    added go build flag to remove symbol and debug info

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 clang -O3 c/code.c -o c/code
-go build -o go/code go/code.go
+go build -ldflags "-s -w" -o go/code go/code.go
 javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
 kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
