120d2affa6bc0db008afcc26c893d55a2443d8ce
Askhat Saiapov <askhat.saiapov@gmail.com>
2024-11-27 01:20:15 +0100

    tail call optimized fibonacci
    credit @MikaelFangel

--- fibonacci/elixir/bench.exs
+++ fibonacci/elixir/bench.exs
@@ -9,8 +9,9 @@ defmodule Bench do
   defp sum_fibs(i, max, total) when i == max, do: total
   defp sum_fibs(i, max, total), do: sum_fibs(i + 1, max, total + fib(i))
 
-  defp fib(n) when n <= 1, do: n
-  defp fib(n), do: fib(n - 1) + fib(n - 2)
+  defp fib(n, current \\ 0, next \\ 1)
+  defp fib(n, current, _next) when n == 0, do: current
+  defp fib(n, current, next), do: fib(n - 1, next, current + next)
 end
 
 Bench.run()
