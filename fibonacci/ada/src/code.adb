b8a8921052f72007c8c813ebde7ee9df05cd7591
aparaev <nabrosok-hd@yandex.ru>
2024-11-27 17:57:12 +0700

    fix typo

--- fibonacci/ada/src/code.adb
+++ fibonacci/ada/src/code.adb
@@ -3,7 +3,7 @@ with Ada.Command_Line; use Ada.Command_Line;
 
 procedure Code is
 
-   function Fibbonaci (N : Integer) return Integer is
+   function Fibonacci (N : Integer) return Integer is
    begin
       if N = 0 then
          return 0;
@@ -13,15 +13,15 @@ procedure Code is
          return 1;
       end if;
 
-      return Fibbonaci (N - 1) + Fibbonaci (N - 2);
-   end Fibbonaci;
+      return Fibonacci (N - 1) + Fibonacci (N - 2);
+   end Fibonacci;
 
    U : constant Integer := Integer'Value (Argument (1));
    R : Integer := 0;
 
 begin
    for I in 1 .. U loop
-      R := R + Fibbonaci (I);
+      R := R + Fibonacci (I);
    end loop;
 
    Put_Line (R'Image);
