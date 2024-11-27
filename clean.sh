03b414eea4f7e77344d3f6c2355fd4e54403f511
aparaev <nabrosok-hd@yandex.ru>
2024-11-27 17:10:10 +0700

    update scripts

--- clean.sh
+++ clean.sh
@@ -4,3 +4,4 @@ rm jvm/code.class
 rm -r rust/target
 rm -rf kotlin/code.jar
 rm dart/code
+rm -r ada/obj ada/bin
--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
-clang -O3 c/code.c -o c/code
+gcc -O3 c/code.c -o c/code
 go build -o go/code go/code.go
 javac jvm/code.java
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
 kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
 #kotlinc-native -include-runtime kotlin/code.kt -d kotlin/code
 dart compile exe dart/code.dart -o dart/code --target-os=macos
+alr -C ada/ build --release
