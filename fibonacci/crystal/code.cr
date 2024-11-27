24e12877ca4cabbd46781d12faeb7548a760cd9f
Tamnac <iberoo33@gmail.com>
2024-11-27 02:36:15 -0800

    use wrapping math

--- fibonacci/crystal/code.cr
+++ fibonacci/crystal/code.cr
@@ -2,7 +2,7 @@ u = ARGV[0].to_i
 r = 0
 
 (1...u).each do |i|
-  r += fibonacci(i)
+  r &+= fibonacci(i)
 end
 
 puts r
@@ -11,6 +11,6 @@ def fibonacci(n : Int32) : Int32
   case n
     when 0 then 0
     when 1 then 1
-    else fibonacci(n - 1) + fibonacci(n - 2)
+    else fibonacci(n &- 1) &+ fibonacci(n &- 2)
   end
 end
