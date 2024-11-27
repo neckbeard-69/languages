228ae206fcbdc9784d95dc6c89a4278ea65a325f
Francisco Sainz <pacoswilliams@gmail.com>
2024-11-27 11:58:37 -0600

    Added compiled bun to the tests

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 clang -O3 c/code.c -o c/code
 go build -o go/code go/code.go
 javac jvm/code.java
+bun build --bytecode --compile js/code.js --outfile js/bun
 RUSTFLAGS="-Zlocation-detail=none" cargo +nightly build --manifest-path rust/Cargo.toml --release
 kotlinc -include-runtime kotlin/code.kt -d kotlin/code.jar
 #kotlinc-native -include-runtime kotlin/code.kt -d kotlin/code
