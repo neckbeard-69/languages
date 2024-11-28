ec7ca7e7caa7218a1f1016585ba874c0564ae16f
Sergei Giniatulin <kornburn@gmail.com>
2024-11-28 14:53:44 +0200

    Update code.d
    nicer code for fibonacci

--- fibonacci/d/code.d
+++ fibonacci/d/code.d
@@ -2,8 +2,8 @@ import std.stdio: writeln;
 import std.conv: to;
 
 int fibonacci(int n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
+  if (n < 2)
+      return n;
   return fibonacci(n-1) + fibonacci(n-2);
 }
 
