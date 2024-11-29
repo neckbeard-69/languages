39ecbf8992e05abe0da0b847b8a86a15c90ffb7b
Angelos Karatsidis <karatsidis@gmail.com>
2024-11-29 21:59:24 +0100

    PEP8 styling fixes

--- fibonacci/py/code.py
+++ fibonacci/py/code.py
@@ -1,17 +1,20 @@
 import sys
 
+
 def fibonacci(n):
-  if (n == 0):
-    return 0
-  if (n == 1):
-    return 1
-  return fibonacci(n-1) + fibonacci(n-2)
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
 
 def main():
     u = int(sys.argv[1])
     r = 0
     for i in range(1, u):
-      r += fibonacci(i)
+        r += fibonacci(i)
     print(r)
+
 
 main()
