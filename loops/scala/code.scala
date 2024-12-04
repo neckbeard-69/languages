dc625b4cb44e03e21830778728b6ca72d3ec2816
虎鸣 <hepin.p@alibaba-inc.com>
2024-12-04 12:55:12 +0800

    chore: optimize scala performance

--- loops/scala/code.scala
+++ loops/scala/code.scala
@@ -1,3 +1,5 @@
+//> using scala 3.6.1
+
 package jvm
 
 import java.util.Random
@@ -7,8 +9,11 @@ def main(number: String): Unit =
   val u = number.toInt // Get an input number from the command line
   val r = Random().nextInt(10000) // Get a random number 0 <= r < 10k
   val a = new Array[Int](10000) // Array of 10k elements initialized to 0
-  for i <- 0 until 10000 do  // 10k outer loop iterations
-    for j <- 0 until 100000 do   // 100k inner loop iterations, per outer loop iteration
+  var i, j = 0
+  while i < 10000 do // 10k outer loop iterations
+    while j < 100000 do // 100k inner loop iterations, per outer loop iteration
       a(i) = a(i) + j % u // Simple sum
+      j += 1
     a(i) += r // Add a random value to each element in array
+    i += 1
   println(a(r)) // Print out a single element from the array
