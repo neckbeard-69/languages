12dc0613de3d773cfbe9322d1596c6e75cb72f7e
PEZ <pez@pezius.com>
2024-12-04 19:01:47 +0100

    Revert "Replace recursion with iteration"
    This reverts commit b2f2d100c8d58ddb48c36730c12b83618821cd2b.

--- fibonacci/clojure/code.clj
+++ fibonacci/clojure/code.clj
@@ -1,19 +1,16 @@
 (ns code
   (:gen-class))
 
-(defn- fibonacci ^long [^long n]
-  (loop [a 0
-         b 1
-         i 0]
-    (if (< i n)
-      (recur b (+ a b) (inc i))
-      a)))
-
 (defn -main [& args]
-  (let [^long u (parse-long (first args))
+  (let [fibonacci (fn fib ^long [^long n]
+                    (if (<= n 1)
+                      n
+                      (+ (long (fib (- n 1)))
+                         (long (fib (- n 2))))))
+        ^long u (parse-long (first args))
         ^long r (loop [i 1
                        sum 0]
                   (if (< i u)
-                    (recur (inc i) (+ sum (fibonacci i)))
+                    (recur (inc i) (long (+ sum (long (fibonacci i)))))
                     sum))]
     (println r)))
