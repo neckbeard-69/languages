8cd5231f9e68228373429d40b508226f1be9c126
Finn <34718806+FinnDore@users.noreply.github.com>
2024-12-05 20:25:32 +0000

    comments

--- loops/cobol/main.cbl
+++ loops/cobol/main.cbl
@@ -3,38 +3,37 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-      * Input number from command line
        01 INPUT-NUMBER              PIC 9(05) VALUE ZERO.
-      * Random number
        01 RANDOM-NUMBER             PIC 9(06) VALUE zero.
-      * Array of 10,000 elements
+      * Array of 10k elements initialized to 0
        01 ARRAY                     OCCURS 10000 TIMES INDEXED BY IDX.
            05 ELEMENT               PIC S9(10) COMP VALUE ZERO.
-      * Loop counters
        01 I                         PIC 9(05) VALUE 1.
        01 J                         PIC 9(06) VALUE 1.
-      * Temporary variables
        01 MODULO-RESULT             PIC S9(05) COMP.
        01 ELEMENT-RESULT            PIC S9(10) COMP.
 
        PROCEDURE DIVISION.
        MAIN-PROCESS.
+      * Get an input number from the command line
            Accept INPUT-NUMBER from command-line 
+      * Get a random number 0 <= r < 10k
            COMPUTE RANDOM-NUMBER = FUNCTION RANDOM * 10000
 
-      * Outer loop: 10,000 iterations
+      * 10k outer loop iterations
            PERFORM VARYING I FROM 1 BY 1 UNTIL I > 10000
-      * Inner loop: 100,000 iterations
+      * 100k inner loop iterations, per outer loop iteration
                PERFORM VARYING J FROM 1 BY 1 UNTIL J > 100000
+      *  Simple sum
                    ADD function MOD(J, INPUT-NUMBER)
                    TO ELEMENT OF ARRAY (I)
                END-PERFORM
-      * Add random number to the current element
+      * Add a random value to each element in array
                ADD RANDOM-NUMBER TO ELEMENT OF ARRAY (I)
            END-PERFORM.
 
-      * Retrieve and print the element at the random index
            MOVE ELEMENT OF ARRAY (RANDOM-NUMBER + 1) TO ELEMENT-RESULT
+      * Print out a single element from the array
            DISPLAY ELEMENT-RESULT
            STOP RUN.
 
