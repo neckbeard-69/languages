f0d12e319bc6b0509f234d1f8fafcdb15558b05e
Daniel Obiorah <beetroit3266@gmail.com>
2024-12-05 15:38:51 +0100

    Update: speed improvements to loops implementation.

--- loops/nim/code.nim
+++ loops/nim/code.nim
@@ -1,20 +1,21 @@
 import std/[random, os, strutils]
 
-# Get an input number from the command line
-let u: uint32 = parseInt(paramStr(1)).uint32
+let 
+    # Get an input number from the command line
+    u: uint32 = parseInt(paramStr(1)).uint32
 
-# Get a random number 0 <= r < 10k
-var r:uint32 = rand(10000).uint32
+    # Get a random number 0 <= r < 10k
+    r:uint32 = rand(10000).uint32
 
 # Array of 10k elements initialized to 0
-var a: seq[uint32]  = newSeq[uint32](10000)
+var a: array[10000, uint32] 
 
 # 10k outer loop iterations
 for i in 0..9999:
     # 100k inner loop iterations, per outer loop iteration
-    for j in 0..99999:
+    for j in 0..99999.uint32:
         # Simple sum
-        a[i] = a[i] + j.uint32 mod u
+        a[i] = a[i] + j mod u
     # Add a random value to each element in array
     a[i] = a[i] + r
 
