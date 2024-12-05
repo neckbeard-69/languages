6ac1c7a68b538d82f5cd2de7436dab744a390523
Sergei Giniatulin <kornburn@gmail.com>
2024-12-05 18:50:43 +0200

    Updated types
    Moved to ulong (lower type conversions in the loop) and added explicit immutable type to unchanged variables.
    Also added FLTO to the compilation flag

--- loops/d/code.d
+++ loops/d/code.d
@@ -3,14 +3,14 @@ import std.stdio: writeln;
 import std.random: Random, unpredictableSeed, uniform;
 
 void main (string[] args) {
-  int u = args[1].to!int;               // Get an input number from the command line
+  immutable u = args[1].to!ulong;               // Get an input number from the command line
   auto rnd = Random(unpredictableSeed);
-  int r = uniform(0, 10_000, rnd);              // Get a random integer 0 <= r < 10k
-  int[10_000] a;              // Array of 10k elements initialized to 0
+  immutable r = uniform(0, 10_000, rnd);              // Get a random ulongeger 0 <= r < 10k
+  ulong[10_000] a;              // Array of 10k elements initialized to 0
   foreach(i; 0..10_000) {
     foreach(j; 0..100_000)
       a[i] += j%u;               // Simple sum
     a[i] += r;                         // Add a random value to each element in array
   }
-  writeln(a[r]);                // Print out a single element from the array
+  writeln(a[r]);                // Prulong out a single element from the array
 }
