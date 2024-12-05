542b8672b5127667eb02feffdda86dcbb40f65d1
aqui <aquilesffoliveira@gmail.com>
2024-12-05 17:38:32 -0300

    added fsharp to loops

---
+++ loops/fsharp/Program.fs
@@ -0,0 +1,18 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length < 1 then
+        printfn ""
+        1
+    else
+        let u = int argv.[0]
+        let rnd = Random()
+        let r = rnd.Next(10000)
+        let a = Array.zeroCreate<int> 10000
+        for i in 0..9999 do
+            for j in 0..99999 do
+                a.[i] <- a.[i] + j % u
+            a.[i] <- a.[i] + r
+        printfn "%d" a.[r]
+        0
