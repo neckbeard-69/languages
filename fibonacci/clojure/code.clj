5402067c8e4db9a6e8074072109617de7a003b31
PEZ <pez@pezius.com>
2024-12-05 18:29:11 +0100

    Use unchecked-math setting and clean up some type hinting

--- fibonacci/clojure/code.clj
+++ fibonacci/clojure/code.clj
@@ -1,16 +1,18 @@
 (ns code
   (:gen-class))
 
+(set! *unchecked-math* :warn-on-boxed)
+
 (defn -main [& args]
-  (let [fibonacci (fn fib ^long [^long n]
-                    (if (<= n 1)
-                      n
-                      (+ (long (fib (- n 1)))
-                         (long (fib (- n 2))))))
-        ^long u (parse-long (first args))
-        ^long r (loop [i 1
-                       sum 0]
-                  (if (< i u)
-                    (recur (inc i) (long (+ sum (long (fibonacci i)))))
-                    sum))]
+  (let [fibonacci ^long (fn fib ^long [^long n]
+                          (if (<= n 1)
+                            n
+                            (+ (long (fib (- n 1)))
+                               (long (fib (- n 2))))))
+        u (long (parse-long (first args)))
+        r (loop [i 1
+                 sum 0]
+            (if (< i u)
+              (recur (inc i) (+ sum (long (fibonacci i))))
+              sum))]
     (println r)))
--- loops/clojure/code.clj
+++ loops/clojure/code.clj
@@ -1,10 +1,12 @@
 (ns code
   (:gen-class))
 
+(set! *unchecked-math* :warn-on-boxed)
+
 (defn -main [& args]
-  (let [^long u (parse-long (first args)) ; Get an input number from the command line
-        ^long r (rand-int 10000) ; Get a random number 0 <= r < 10k
-        ^longs a (long-array 10000)] ; Array of 10k elements initialized to 0
+  (let [u (long (parse-long (first args))) ; Get an input number from the command line
+        r (long (rand-int 10000)) ; Get a random number 0 <= r < 10k
+        a (long-array 10000)] ; Array of 10k elements initialized to 0
     (loop [i 0]
       (when (< i 10000) ; 10k outer loop iterations
         (loop [j 0]
