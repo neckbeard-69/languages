9107173ec3c79fb32beff4b0f5ed78be32908871
Angelos Karatsidis <karatsidis@gmail.com>
2024-12-06 23:34:28 +0100

    Revert "revert vectorization to preserve consistency with other language implementations (significantly slower)"
    This reverts commit 1b56044d262491ee8718e0b3dc58880825d8bcee.

--- loops/py/code.py
+++ loops/py/code.py
@@ -3,8 +3,8 @@ import random
 
 
 def main():
-    u = 40 #int(sys.argv[1])  # Get an input number from the command line
-    r = 5000 #random.randint(0, 10000)  # Get a random number 0 <= r < 10k
+    u = int(sys.argv[1])  # Get an input number from the command line
+    r = random.randint(0, 10000)  # Get a random number 0 <= r < 10k
     a = [0] * 10000  # Array of 10k elements initialized to 0
     for i in range(10000):  # 10k outer loop iterations
         for j in range(100000):  # 100k inner loop iterations, per outer loop iteration
