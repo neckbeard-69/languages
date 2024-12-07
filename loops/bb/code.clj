f99a5720996912427a7292e79dc5ae525b3ac6dd
PEZ <pez@pezius.com>
2024-12-07 22:52:45 +0100

    Babashka: Use a functional loops implementation (reduce)

--- loops/bb/code.clj
+++ loops/bb/code.clj
@@ -1,18 +1,15 @@
-(ns code
-  (:gen-class))
+(defn main [u]
+  (let [r (rand-int 10000) ; Get a random number 0 <= r < 10k
+        v' (vec (repeat 10000 0)) ; Vector of 10k elements initialized to 0
+        v (reduce (fn [v i]
+                    (let [inner-sum (reduce
+                                     (fn [sum j]
+                                       (+ sum (rem j u))) ; Simple sum
+                                     (v i)
+                                     (range 100000))] ; 100k inner loop iterations, per outer loop iteration
+                      (assoc v i (+ inner-sum r)))) ; Add a random value to each element in array
+                  v'
+                  (range 10000))] ; 10k outer loop iterations
+    (println (nth v r)))) ; Print out a single element from the array
 
-(set! *unchecked-math* :warn-on-boxed)
-
-(defn -main [& args]
-  (let [u (long (parse-long (first args))) ; Get an input number from the command line
-        r (long (rand-int 10000)) ; Get a random number 0 <= r < 10k
-        a (long-array 10000)] ; Array of 10k elements initialized to 0
-    (loop [i 0]
-      (when (< i 10000) ; 10k outer loop iterations
-        (loop [j 0]
-          (when (< j 100000) ; 100k inner loop iterations, per outer loop iteration
-            (aset a i (unchecked-add (aget a i) (rem j u))) ; Simple sum
-            (recur (unchecked-inc j))))
-        (aset a i (unchecked-add (aget a i) r)) ; Add a random value to each element in array
-        (recur (unchecked-inc i))))
-    (println (aget a r)))) ; Print out a single element from the array
+(main (-> *command-line-args* first parse-long))  ; Get an input number from the command line
