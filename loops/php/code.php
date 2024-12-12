3f642b4f9c7efdcccb041b33127af1dfb0da906e
KsaR <KsaR99@users.noreply.github.com>
2024-12-12 01:07:02 +0100

    Revert "Enable strict types."
    This reverts commit 51ba8ddd7e4b30efb38d347f2b9d5ae7a90d15d6.

--- loops/php/code.php
+++ loops/php/code.php
@@ -1,14 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
-$u = (int) $argv[1];                   // Get an input number from the command line
-$r = rand(0, 10_000);                  // Get a random number 0 <= r < 10k
-$a = array_fill(0, 10_000, 0);         // Array of 10k elements initialized to 0
-for ($i = 0; $i < 10_000; $i++) {      // 10k outer loop iterations
-    for ($j = 0; $j < 100_000; $j++) { // 100k inner loop iterations, per outer loop iteration
-        $a[$i] += $j % $u;             // Simple sum
+$u = (int) $argv[1];                                # Get an input number from the command line
+$r = rand(0, 10000);                                # Get a random number 0 <= r < 10k
+$a = array_fill(0, 10000, 0);    # Array of 10k elements initialized to 0
+for ($i = 0; $i < 10000; $i++) {                    # 10k outer loop iterations
+    for ($j = 0; $j < 100000; $j++) {               # 100k inner loop iterations, per outer loop iteration
+        $a[$i] += $j%$u;                    # Simple sum
     }
-    $a[$i] += $r;                      // Add a random value to each element in array
+    $a[$i] += $r;                                   # Add a random value to each element in array
 }
-echo $a[$r];                           // Print out a single element from the array
+echo $a[$r];                                        # Print out a single element from the array
