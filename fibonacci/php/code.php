39776f9b0bd7bad245b8e909120d35104ef32c6c
KsaR <KsaR99@users.noreply.github.com>
2024-12-12 01:06:55 +0100

    Revert "Enable strict mode."
    This reverts commit c78640335813cdcca28b5692af6e03c6b6ea2d82.

--- fibonacci/php/code.php
+++ fibonacci/php/code.php
@@ -1,19 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
-function fibonacci(int $n): int                   // Have a function that recursively compute a fibonacci number with this naive algorithm
+function fibonacci(int $n): int
 {
-    if ($n <= 1) {                                // Base case for input 0, 1.
-    	return $n;
-    }
-
-    return fibonacci($n - 1) + fibonacci($n - 2); // Must make two recursive calls for each non-base invocation
+    if ($n == 0) return 0;
+    if ($n == 1) return 1;
+    return fibonacci($n - 1) + fibonacci($n - 2);
 }
 
-$u = (int) $argv[1];                              // Get exactly one numeric value from the command line
-$r = 0;                                           // Create variable to store sum
-for ($i = 1; $i < $u; $i++) {                     // Loop 1...u times
-    $r += fibonacci($i);                          // Sum all fibonacci numbers 1...u
+$u = (int) $argv[1];
+$r = 0;
+for ($i = 0; $i < $u; $i++) {
+    $r += fibonacci($i);
 }
-echo $r;                                          // Print out the single, numeric sum
+echo $r;
