ccd44c8b56b7e06b9597d34928d6b055fc64dfd7
KsaR <KsaR99@users.noreply.github.com>
2024-12-12 01:38:32 +0100

    Regular  levenshtein
    Use of regular implementation instead of builtin.

--- levenshtein/php/code.php
+++ levenshtein/php/code.php
@@ -2,6 +2,37 @@
 
 declare(strict_types=1);
 
+function levenshtein_distance(string $str1, string $str2): int
+{
+    $m = strlen($str1);
+    $n = strlen($str2);
+  
+    // Create a matrix to store distances
+    $matrix = [];
+
+    // Initialize first row and column
+    for ($i = 0; $i <= $m; $i++) {
+        $matrix[$i][0] = $i;
+    }
+    for ($j = 0; $j <= $n; $j++) {
+        $matrix[0][$j] = $j;
+    }
+ 
+    // Compute Levenshtein distance
+    for ($i = 1; $i <= $m; $i++) {
+        for ($j = 1; $j <= $n; $j++) {
+            $cost = (int) $str1[$i - 1] != $str2[$j - 1];
+            $matrix[$i][$j] = min(
+                $matrix[$i - 1][$j] + 1,        // Deletion
+                $matrix[$i][$j - 1] + 1,        // Insertion
+                $matrix[$i - 1][$j - 1] + $cost // Substitution
+            );
+        }
+    }
+  
+  return $matrix[$m][$n];
+}
+
 // Main function to mimic C program's behavior
 function main(array $args): void
 {
@@ -10,9 +41,9 @@ function main(array $args): void
 
     foreach ($args as $i => $str1) {
         foreach ($args as $j => $str2) {
-            if ($i !== $j) {
-                $distance = levenshtein($str1, $str2);
-                if ($minDistance === -1 || $minDistance > $distance) {
+            if ($i != $j) {
+                $distance = levenshtein_distance($str1, $str2);
+                if ($minDistance == -1 || $minDistance > $distance) {
                     $minDistance = $distance;
                 }
 
